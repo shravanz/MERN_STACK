43a8575e921c692862c1d2900153fc3bb0110703
shravanz <shravanzolgikar@yahoo.in>
2018-04-19 21:31:35 +0530

    Added profileRoute

--- routes/apis/profile.js
+++ routes/apis/profile.js
@@ -1,6 +1,13 @@
 //Module Dependencies
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+//Load Profile Model
+const Profile = require("../../model/Profile");
+//Load User Model
+const User = require("../../model/User");
 
 // set the routes
 
@@ -8,5 +15,27 @@ const router = express.Router();
 // @desc    Tests profile route
 // @access  public
 router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
+
+// @route   GET api/profile
+// @desc    Get Current User Profile
+// @access  Private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user._id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => {
+        return res.status(404).json(err);
+      });
+  }
+);
 
 module.exports = router;
